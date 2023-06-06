db4cb3b7235b7c734c8975590ca01392f419a100
pjserol <pjserol@gmail.com>
2023-06-06 17:24:55 +1000

    record game winner + tests

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -99,11 +99,12 @@ func TestPlayMoveSavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "r",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "r",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 }
 
@@ -130,6 +131,7 @@ func TestPlayMoveEmitted(t *testing.T) {
 			{Key: "captured-x", Value: "-1"},
 			{Key: "captured-y", Value: "-1"},
 			{Key: "winner", Value: "*"},
+			{Key: "board", Value: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*"},
 		},
 	}, event)
 }
@@ -264,11 +266,12 @@ func TestPlayMove2SavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "b",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "b",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 }
 
@@ -302,7 +305,8 @@ func TestPlayMove2Emitted(t *testing.T) {
 		{Key: "captured-x", Value: "-1"},
 		{Key: "captured-y", Value: "-1"},
 		{Key: "winner", Value: "*"},
-	}, event.Attributes[5:])
+		{Key: "board", Value: "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*"},
+	}, event.Attributes[6:])
 }
 
 func TestPlayMove3(t *testing.T) {
@@ -374,10 +378,11 @@ func TestPlayMove3SavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(ctx, "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Index: "1",
-		Board: "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:  "r",
-		Black: bob,
-		Red:   carol,
+		Index:  "1",
+		Board:  "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:   "r",
+		Black:  bob,
+		Red:    carol,
+		Winner: "*",
 	}, game1)
 }
